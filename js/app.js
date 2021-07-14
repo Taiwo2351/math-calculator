a053a790b047d64d4a824317cece57ae01b4dcb7
Idowu Taiwo John <idowutaiwo6598@gmail.com>
2021-07-14 08:26:08 +0100

    calculator

--- js/app.js
+++ js/app.js
@@ -15,33 +15,33 @@ function addition(){
   let num1 = document.getElementById("num1").value;
   let num2= document.getElementById("num2").value;
    let sum = (+num1) + (+num2);
-  document.getElementById("display").innerHTML = `<h1>${ sum}<h1>`;
+  document.getElementById("display").innerHTML = `<h1>${ sum}</h1>`;
 }
 
 function subtraction(){
   let num1 = document.getElementById("num1").value;
   let num2= document.getElementById("num2").value;
    let sum = num1 - num2;
-  document.getElementById("display").innerHTML = `<h1>${ sum}<h1>`;
+  document.getElementById("display").innerHTML = `<h1>${ sum}</h1>`;
 }
 
 function multiplication(){
   let num1 = document.getElementById("num1").value;
   let num2= document.getElementById("num2").value;
    let sum = num1 * num2;
-  document.getElementById("display").innerHTML = `<h1>${ sum}<h1>`;
+  document.getElementById("display").innerHTML = `<h1>${ sum}</h1>`;
 }
 
 function division(){
   let num1 = document.getElementById("num1").value;
   let num2= document.getElementById("num2").value;
    let sum = num1 / num2;
-  document.getElementById("display").innerHTML = `<h1>${ sum}<h1>`;
+  document.getElementById("display").innerHTML = `<h1>${ sum}</h1>`;
 }
 
 function remainder(){
   let num1 = document.getElementById("num1").value;
   let num2= document.getElementById("num2").value;
    let sum = num1 % num2;
-  document.getElementById("display").innerHTML = `<h1>${ sum}<h1>`;
+  document.getElementById("display").innerHTML = `<p>${ sum}</p>`;
 }
